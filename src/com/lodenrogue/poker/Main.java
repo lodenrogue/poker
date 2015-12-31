aba5c27dea1b0ad22faff1d8d7ad1a607d88a93e
Miguel Hernandez <phyzix5761@gmail.com>
2015-12-31 16:01:51 -0500

    Added deck
    Deck class handles creating a deck, shuffling a deck, and dealing cards

--- src/com/lodenrogue/poker/Main.java
+++ src/com/lodenrogue/poker/Main.java
@@ -1,7 +1,5 @@
 package com.lodenrogue.poker;
 
-import com.lodenrogue.poker.game.Rank;
-
 public class Main {
 
 	public static void main(String[] args) {
---
+++ src/com/lodenrogue/poker/game/Deck.java
@@ -0,0 +1,44 @@
+package com.lodenrogue.poker.game;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Deck {
+	private static ArrayList<Card> protoDeck;
+	private ArrayList<Card> userDeck;
+	private Random rand;
+
+	static {
+		protoDeck = new ArrayList<>();
+		for (Rank rank : Rank.values()) {
+			for (Suit suit : Suit.values()) {
+				protoDeck.add(new Card(rank, suit));
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public Deck() {
+		rand = new Random();
+		userDeck = (ArrayList<Card>) protoDeck.clone();
+	}
+
+	public void shuffle() {
+		@SuppressWarnings("unchecked")
+		ArrayList<Card> tmpUserDeck = (ArrayList<Card>) protoDeck.clone();
+
+		userDeck.clear();
+		while (tmpUserDeck.size() > 0) {
+			int rCard = rand.nextInt(tmpUserDeck.size());
+			userDeck.add(tmpUserDeck.remove(rCard));
+		}
+	}
+
+	public Card deal() {
+		if (userDeck.size() > 0) {
+			return userDeck.remove(0);
+		}
+		return null;
+	}
+
+}
